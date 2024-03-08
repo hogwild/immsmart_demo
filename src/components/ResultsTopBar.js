837f9f1e48ee730060fcb661f00456829faa36b0
hogwild <guxianbinmail@gmail.com>
2024-03-08 11:26:45 +0800

    Merge branch 'main' of https://github.com/hogwild/immsmart_demo

--- src/components/ResultsTopBar.js
+++ src/components/ResultsTopBar.js
@@ -7,7 +7,11 @@ const ResultsTopBar = (props) => {
     <div className="d-flex justify-content-between align-items-center flex-column flex-md-row mb-4">
       <div className="me-3">
         <p className="mb-3 mb-md-0">
+<<<<<<< HEAD
           <strong>{props.nums}</strong>
+=======
+          <strong>12</strong>
+>>>>>>> d13d1436d09edcff7f3604d47c6851da3a463c0a
           &nbsp;results found
         </p>
       </div>
